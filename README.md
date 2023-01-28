bdc560d8da4c7c6b887f0e58d39dfaf169a542a0
volodink <volodin.konstantin@gmail.com>
2023-01-28 13:44:09 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Spring Demo App
+
+Spring Demo App description
+
+# Запуск проекта
+
+Для непрерывной сборки проекта без постоянного запуска:
+
+```bash
+    gradle build -continuous
+```
+
+Для запуска сервиса:
+
+```bash
+    gradle bootRun
+```
+
+Проверяем работоспособность:
+
+```bash
+    open http://localhost:8080/hello
+```
---
+++ src/main/java/edu/penzgtu/springdemoapp/SpringDemoAppApplication.java
@@ -0,0 +1,11 @@
+package edu.penzgtu.springdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAppApplication.class, args);
+	}
+}
